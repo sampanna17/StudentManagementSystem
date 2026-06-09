9bf9a9c932a52ee796964793d6bda2f761bf0f67
sampanna17 <sampannapiya13@gmail.com>
2026-06-09 10:44:10 +0545

    Sutdent Model

--- src/main/java/com/studentmanagementsystem/model/Student.java
+++ src/main/java/com/studentmanagementsystem/model/Student.java
@@ -1,4 +1,39 @@
 package com.studentmanagementsystem.model;
 
 public class Student {
+    private int student_id;
+    private String student_name;
+    private int marks;
+    private String grade;
+
+    public Student(int student_id, String student_name, int marks){
+        this.student_id = student_id;
+        this.student_name = student_name;
+        this.marks = marks;
+        this.grade = calculateGrade(marks);
+    }
+
+    private String calculateGrade(int marks){
+        if (marks >= 80 ) return "A";
+        else if (marks >= 60 ) return "B";
+        else if (marks >= 50 ) return "C";
+        else if (marks >=40) return "D";
+        else return "F";
+    }
+
+    public int getId() {return student_id; }
+    public String getName() {return student_name;}
+    public int getMarks() {return marks;}
+    public String getGrade() {return grade;}
+
+    public void setMarks(int marks){
+        this.marks =  marks;
+        this.grade = calculateGrade(marks);
+    }
+
+    @Override
+    public String toString(){
+        return "ID : " + student_id + " | Name : " + student_name + " | Marks : " + marks + " | Grade: " + grade;
+    }
+
 }
